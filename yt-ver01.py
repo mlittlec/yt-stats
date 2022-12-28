6bee4f22b8622182db0a62b930351aaa45d7bba5
Martin Littlecott <mlittlec@hotmail.com>
2022-12-28 22:02:13 +0000

    Correct typos

--- yt-ver01.py
+++ yt-ver01.py
@@ -4,6 +4,7 @@ api_key = <Enter_Your_Key_Here>
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
+# Return basic YouTube statistics for a user name
 request = youtube.channels().list(part='statistics', forUsername='schafer5')
 
 response = request.execute()
