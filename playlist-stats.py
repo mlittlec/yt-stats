0a8c36d1f4c3504389b66328b3a662958c7082e7
Martin Littlecott <mlittlec@hotmail.com>
2022-12-30 20:08:19 +0000

    Add some Playlist statistics retrieval code

---
+++ playlist-stats.py
@@ -0,0 +1,15 @@
+import os
+from googleapiclient.discovery import build
+
+api_key = os.environ.get('YT_API_KEY')
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+request = youtube.channels().list(
+    part='contentDetails, statistics',
+    forUsername='schafer5'
+)
+
+response = request.execute()
+
+print(response)
