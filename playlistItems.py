af873a29c0d8ef31b3be944bf1b20e03eea8a6cd
Martin Littlecott <mlittlec@hotmail.com>
2023-01-03 21:03:05 +0000

    Add code to return the Playlist's video id's

--- playlistItems.py
+++ playlistItems.py
@@ -5,10 +5,10 @@ api_key = os.environ.get('YT_API_KEY')
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-pl_request = youtube.playlists().list(
+pl_request = youtube.playlistItems().list(
 #request = youtube.channels().list(
-    part="contentDetails, snippet",
-    channelId="<channel-id-queried>"
+    part="contentDetails",
+    playlistId="<channel-id-queried>"
     #forUsername="schafer5"
 )
 
@@ -17,5 +17,6 @@ pl_response = pl_request.execute()
 # print(pl_response) # Print out the first 5 records returned
 
 for item in pl_response['items']:
-    print(item)
+    vid_id = item['contentDetails']['videoId']
+    print(vid_id)
     print()
